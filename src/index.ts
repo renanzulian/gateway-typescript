f85b5fe1cb3e6e3e720a82f56e77559542e22209
Renan Zulian <renanzulian@gmail.com>
2020-09-16 08:42:31 -0300

    Adding health check

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import Express from 'express'
 import { userRoutes } from '@app/users/index'
 import { orderRoutes } from '@app/orders/index'
+import HttpStatusCode from '@commons/httpCode'
 
 class App {
+    private liveCheck = { name: 'App is live!', status: 'UP' }
+    private readyCheck = { name: 'App is ready!', status: 'UP' }
     private server: Express.Application;
 
     constructor () {
       this.server = Express()
-      this.middlewares()
+      this.commonMiddlewares()
       this.routes()
+      this.healthCheck()
       this.unhandledRequests()
     }
 
@@ -16,7 +20,7 @@ class App {
       return this.server
     }
 
-    private middlewares () {
+    private commonMiddlewares () {
     }
 
     private routes () {
@@ -24,8 +28,24 @@ class App {
       this.server.use('/orders', orderRoutes)
     }
 
-    private unhandledRequests () {
+    private healthCheck () {
+      this.server.get('/health', (req, res) => {
+        res.json({ status: 'UP', checks: [this.liveCheck, this.readyCheck] })
+      })
 
+      this.server.get('/health/live', (req, res) => {
+        res.json(this.liveCheck)
+      })
+
+      this.server.get('/health/ready', (req, res) => {
+        res.json(this.readyCheck)
+      })
+    }
+
+    private unhandledRequests () {
+      this.server.use('*', (req, res) => {
+        res.sendStatus(HttpStatusCode.NOT_FOUND)
+      })
     }
 }
 
